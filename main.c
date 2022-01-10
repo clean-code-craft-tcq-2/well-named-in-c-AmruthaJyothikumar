aafb481ba1c17ae4f736da505e278db8c5753d43
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2022-01-10 17:27:15 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-
 int numberOfMajorColors =
     sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 
