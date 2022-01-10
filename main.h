b75ab48759450ec8c313c8fa7d8d0d016f109b61
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2022-01-10 17:22:59 +0530

    Update main.h

--- main.h
+++ main.h
@@ -1,19 +1,3 @@
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-
-typedef struct {
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
-} ColorPair;
-
-extern ColorPair Colors;
 
 extern ColorPair GetColorFromPairNumber(int pairNumber);
 extern int GetPairNumberFromColor(const ColorPair* colorPair);
