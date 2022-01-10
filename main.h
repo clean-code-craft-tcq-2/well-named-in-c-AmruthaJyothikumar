9531725f0beff549d7d8f3c9d041f01a7dd779f4
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2022-01-10 17:18:06 +0530

    Update main.h

--- main.h
+++ main.h
@@ -1,7 +1,6 @@
 extern enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 extern enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-const int MAX_COLORPAIR_NAME_CHARS = 16;
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
