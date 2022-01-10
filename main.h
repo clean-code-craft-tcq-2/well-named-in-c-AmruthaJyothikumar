f36563024c491ca6c0cc75015da3b8ab92c40ade
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2022-01-10 16:58:49 +0530

    Update main.h

--- main.h
+++ main.h
@@ -15,3 +15,7 @@ typedef struct {
 } ColorPair;
 
 extern ColorPair Colors;
+
+extern ColorPair GetColorFromPairNumber(int pairNumber);
+extern int GetPairNumberFromColor(const ColorPair* colorPair);
+extern void PrintColorPairToString(const ColorPair* colorPair, char* buffer);
