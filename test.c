56b9b5da2e9a29407402c14c79e4dd4794700947
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2022-01-10 17:25:43 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,22 +1,6 @@
 #include <assert.h>
 #include "main.h"
 
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-
-typedef struct {
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
-} ColorPair;
-
-extern ColorPair Colors;
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 
 void  testColorfromPairNumber (int pairNumber,
